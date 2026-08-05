9bbba073d7312370a8e842b134893a380bac59d4
Cerith Freeman <cerith.elis@gmail.com>
2026-08-05 16:01:10 +0100

    updated 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     <div className="page-contents NotFoundPage">
       <h1 className="PageTitle">404 Page Not Found</h1>
       <p>
-        Sorry, the page you were looking for couldn't be found.
+        Sorry, the page you were looking for could not be found.
         <br />
         Try picking one of the pages from the navigation bar above instead.
       </p>
